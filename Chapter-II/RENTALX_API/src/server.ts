e2b398e98a517dc8cd5d40ea87a4445f5308614f
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-03-25 11:04:36 +0100

    final of Chapter-II

--- Chapter-II/RENTALX_API/src/server.ts
+++ Chapter-II/RENTALX_API/src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
+
+import swagguerUi from 'swagger-ui-express'
 import { router } from "./routes";
+import swaggerFile from './swagger.json'
+
 
 const app = express()
 app.use(express.json())
+app.use("/api-docs", swagguerUi.serve, swagguerUi.setup(swaggerFile))
 app.use(router)
 app.listen(3333, () => {
     console.log("Server is running")
