41e69c474ad14313d878b040a01fd0d777560609
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-04-27 09:28:45 +0100

    Update AuthenticateUserUseCase.spec.ts

--- Chapter-II/RENTALX_API/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ Chapter-II/RENTALX_API/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
@@ -28,6 +29,29 @@ describe("Authenticate User", ()=>{
 
      expect(result).toHaveProperty("token")
   });
+  it("should be able to authenticate an nonexistent user",async ()=>{
+    expect(async()=>{
+      await authenticateUserUseCase.execute({
+        email:"ernesto@gamil.com",
+        password:"1234"
+      });
+    }).rejects.toBeInstanceOf(AppError)
+    });
+    it("should be able to authenticate with incorrect password",async ()=>{
+      expect(async()=>{
+        const user:ICreateUserDTO ={
+          driver_license:"021158",
+          email:"user@gmail.com",
+          password:"1234",
+          name:"User Test Error"
+       } 
+       await createUserUseCase.execute(user)
 
+       await authenticateUserUseCase.execute({
+         email:user.email,
+         password:"incorrect password"
+       })
+      }).rejects.toBeInstanceOf(AppError)
+      });
 
 })
