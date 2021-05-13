0ee6464adb31af9c64a7663e4abefee5a3ab09ad
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-05-13 16:55:38 +0100

    up

--- Chapter-II/RENTALX_API/src/shared/infra/typeorm/index.ts
+++ Chapter-II/RENTALX_API/src/shared/infra/typeorm/index.ts
@@ -17,7 +17,8 @@ export default async():Promise<Connection>=>{
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions,{
-      name:"localhost"
+      name:"localhost",
+      database: process.env.NODE_ENV==='test' ? 'rentx_test': defaultOptions.database
     })
   )
 }
