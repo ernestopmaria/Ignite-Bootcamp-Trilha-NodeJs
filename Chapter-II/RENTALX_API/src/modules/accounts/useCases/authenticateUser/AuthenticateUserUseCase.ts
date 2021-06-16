3f2f08b467bb4dc8011f0eeb2fb0657a6ac6ebf1
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-06-16 13:17:32 +0100

    refreshtokenUseCase

--- Chapter-II/RENTALX_API/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ Chapter-II/RENTALX_API/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,8 +1,11 @@
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from "tsyringe";
+import auth from '../../../../config/auth';
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '../../../../shared/errors/AppError';
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersTokensRepository } from '../../repositories/IUsersTokensRepository';
 
 
 interface IRequest {
@@ -14,17 +17,23 @@ interface IResponse {
         name: string,
         email: string
     },
-    token: string
+    token: string,
+    refresh_token:string
 }
 
 @injectable()
 class AuthenticateUserUseCase {
     constructor(
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject("UsersTokensRepository")
+        private usersTokensRepository:IUsersTokensRepository,
+        @inject ("DayjsDateProvider")
+        private dateProvider:IDateProvider
     ) { }
 
     async execute({ email, password }: IRequest): Promise<IResponse> {
+        const {expires_in_token,secret_refresh_token,secret_token,expires_in_refresh_token,expires_refresh_token_days}= auth
         const user = await this.usersRepository.findByEmail(email)
         if (!user) {
             throw new AppError("Email or password incorrect!")
@@ -33,16 +42,29 @@ class AuthenticateUserUseCase {
         if (!passwordMatch) {
             throw new AppError("Email or password incorrect!")
         }
-        const token = sign({}, "86e679041d7ad8bc167d96ef2540e054", {
+        const token = sign({}, secret_token, {
             subject: user.id,
-            expiresIn: "1d"
+            expiresIn: expires_in_token
+        })
+
+        const refresh_token= sign({email}, secret_refresh_token,{
+            subject:user.id,
+            expiresIn:expires_in_refresh_token
+        })
+
+        const refresh_token_expires_date = this.dateProvider.addDays(expires_refresh_token_days)
+        await this.usersTokensRepository.create({
+            expires_date:refresh_token_expires_date,
+            refresh_token,
+            user_id:user.id
         })
         const tokenReturn: IResponse = {
             user: {
                 name: user.name,
                 email: user.email
             },
-            token
+            token,
+            refresh_token
         }
         return tokenReturn
 
--- Chapter-II/RENTALX_API/src/shared/container/index.ts
+++ Chapter-II/RENTALX_API/src/shared/container/index.ts
@@ -12,6 +12,8 @@ import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImag
 import { CarsImageRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImageRepository'
 import { IRentalsRepository } from '../../modules/rentals/repositories/IRentalsRepository'
 import { RentalsRepository } from '../../modules/rentals/infra/repositories/RentalsRepository'
+import { IUsersTokensRepository } from '../../modules/accounts/repositories/IUsersTokensRepository'
+import { UsersTokensRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository'
 
 container.registerSingleton<ICategoriesRepository>("CategoryRepository", CategoriesRepository)
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository)
@@ -19,3 +21,4 @@ container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository)
 container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImageRepository)
 container.registerSingleton<IRentalsRepository>("RentalsRepository", RentalsRepository)
+container.registerSingleton<IUsersTokensRepository>("UsersTokensRepository", UsersTokensRepository)
