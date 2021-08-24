7118ec41fc12c8185946ef68cec5965a199a626e
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-08-24 16:53:07 +0100

    update serveless

---
+++ Chapter-III/certificationsApp/src/functions/hello.ts
@@ -0,0 +1,14 @@
+
+
+
+export const handle =(event) =>{
+    return{
+        statusCode: 201,
+        body:{
+            message: "Hello World Ignite Serverless",
+        },
+        headers:{
+            "Content-Type": "application/json",
+        },
+    };
+};
